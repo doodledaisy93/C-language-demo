0f4b6c0d92246cdf6f744d98d3de49c97de43cb2
johnwang <doodledaisy93@gmail.com>
2018-07-05 22:48:09 +0800

    分段函数

---
+++ determine statements/Piecewise function.c
@@ -0,0 +1,20 @@
+//·Ö¶Îº¯Êý
+//y=-1;x<0
+//y=0;x=0
+//y=2x;x>0
+#include <stdio.h>
+int main()
+{
+	double x;
+	double y;
+	scanf("%lf",&x);
+	switch(x>=0){
+		case 1:
+			y=2*x;
+		break;
+		default:
+		y=-1;
+		break;
+	}printf("y=%f\n",y);
+	return 0;
+ } 
